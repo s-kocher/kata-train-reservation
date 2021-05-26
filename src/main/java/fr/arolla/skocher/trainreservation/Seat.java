3091a9ae5d2dfad33eaea9677654bf947f9090a9
Sylvain K <skocher@avb202.com>
2021-05-26 17:38:44 +0200

    Initialize with provided classes, pom and readme

---
+++ src/main/java/fr/arolla/skocher/trainreservation/Seat.java
@@ -0,0 +1,18 @@
+package fr.arolla.skocher.trainreservation;
+
+public class Seat {
+
+    public final String coach;
+    public final int seatNumber;
+
+    public Seat(String coach, int seatNumber) {
+        this.coach = coach;
+        this.seatNumber = seatNumber;
+    }
+
+    public boolean equals(Object o) {
+        Seat other = (Seat)o;
+        return coach == other.coach && seatNumber == other.seatNumber;
+    }
+
+}
---
+++ src/test/java/fr/arolla/skocher/trainreservation/TicketOffice.java
@@ -0,0 +1,21 @@
+package fr.arolla.skocher.trainreservation;
+
+import org.junit.jupiter.api.Test;
+
+public class TicketOffice {
+    
+    public Reservation makeReservation(ReservationRequest request) {
+        //TODO: implement this code!
+        return null;
+    }
+
+    public static class TicketOfficeTest {
+
+        @Test
+        public void reserveSeats() {
+            // TODO: Write this code!
+        }
+
+    }
+
+}
