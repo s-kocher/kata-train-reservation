406092035837523c413288976e48fbf03b4ce3ff
Sylvain K <skocher@avb202.com>
2021-06-09 19:25:37 +0200

    book 1 seat in a train globally booked more than 70% is rejected with an empty seats / booking id

--- src/main/java/fr/arolla/skocher/trainreservation/TicketOffice.java
+++ src/main/java/fr/arolla/skocher/trainreservation/TicketOffice.java
@@ -2,9 +2,26 @@ package fr.arolla.skocher.trainreservation;
 
 import java.util.List;
 
+import fr.arolla.skocher.trainreservation.service.TrainDataService;
+
 public class TicketOffice {
 
+    private TrainDataService trainDataService;
+
+    public TicketOffice(TrainDataService trainDataService) {
+        this.trainDataService = trainDataService;
+    }
+
     public Reservation makeReservation(ReservationRequest request) {
+        Train train = trainDataService.getTrainById(request.trainId);
+        if (train.getTrainReservationRate() >= 70) {
+            return new Reservation(
+                request.trainId,
+                List.of(),
+                ""
+            );
+        }
+
         if (request.seatCount == 2) {
             return new Reservation(
                 "express_2000",
--- src/test/java/fr/arolla/skocher/trainreservation/TicketOfficeTest.java
+++ src/test/java/fr/arolla/skocher/trainreservation/TicketOfficeTest.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import fr.arolla.skocher.trainreservation.service.TrainDataService;
+import fr.arolla.skocher.trainreservation.service.TrainDataServiceMock;
+
 public class TicketOfficeTest {
 
     @Test
     public void should_a_1_seat_reservation_in_an_empty_coach_be_approved() {
-        TicketOffice ticketOffice = new TicketOffice();
+        TicketOffice ticketOffice = new TicketOffice(new TrainDataServiceMock());
 
         Reservation reservation = ticketOffice.makeReservation(
             new ReservationRequest("express_2000", 1)
@@ -25,7 +28,7 @@ public class TicketOfficeTest {
 
     @Test
     public void should_a2_seats_reservation_in_an_empty_coach_be_approved() {
-        TicketOffice ticketOffice = new TicketOffice();
+        TicketOffice ticketOffice = new TicketOffice(new TrainDataServiceMock());
 
         Reservation reservation = ticketOffice.makeReservation(
             new ReservationRequest("express_2000", 2)
@@ -37,6 +40,22 @@ public class TicketOfficeTest {
                 new Seat("A", 1),
                 new Seat("A", 2)),
             "75bcd15"
+        );
+        Assertions.assertEquals(expectReservation, reservation);
+    }
+
+    @Test
+    public void should_a_1_seat_reservation_in_a_more_than_70_booked_train_be_rejected() {
+        TicketOffice ticketOffice = new TicketOffice(new TrainDataServiceMock());
+
+        Reservation reservation = ticketOffice.makeReservation(
+            new ReservationRequest("tgv_100", 1)
+        );
+
+        Reservation expectReservation = new Reservation(
+            "tgv_100",
+            List.of(),
+            ""
         );
         Assertions.assertEquals(expectReservation, reservation);
     }
